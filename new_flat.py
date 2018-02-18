1ac5b82fa1b72236b9ab88c450d6d74535973e3b
Pavel Nedrigailov <p_nedrigailov@wargaming.net>
2018-02-18 18:15:09 +0300

    Parameters config simplified

--- new_flat.py
+++ new_flat.py
@@ -24,12 +24,12 @@
 """
 
 import json
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass
+from os.path import isfile
 from typing import List
-from urllib.parse import parse_qs, urlparse, urlencode, urlunparse
+from urllib.parse import parse_qs, urlparse
 
 import requests
-from os.path import isfile
 
 from constants import BASE_API_URL, URL_PARAMETERS, URL_PARAMS_FILE
 
@@ -46,19 +46,6 @@ def filter_new_flats(flats: List) -> List:
     return []
 
 
-@dataclass
-class QueryParams:
-    rent_type: List[str]
-    price_min: int
-    price_max: int
-    currency: str
-    bounds_lb_lat: str
-    bounds_lb_long: str
-    bounds_rt_lat: str
-    bounds_rt_long: str
-    page: int = 1
-
-
 @dataclass()
 class QueryUrl:
     url_base: str
@@ -71,49 +58,39 @@ class Flats:
     coordinate_long: float
 
 
-class ConfigHandler:
+class SearchConfig:
     @staticmethod
-    def update_url_params(query_params: QueryParams) -> None:
+    def write(query_params: dict) -> None:
         file_params = 'w' if isfile(URL_PARAMS_FILE) else 'x'
         with open(URL_PARAMS_FILE, file_params) as f:
-            json.dump(asdict(query_params), f)
+            json.dump(query_params, f)
 
 
     @staticmethod
-    def read_url_params() -> QueryParams:
+    def read() -> dict:
         with open(URL_PARAMS_FILE) as f:
             params = json.load(f)
-        return QueryParams(**params)
+        return params
 
 
 class Onliner:
     """ Send proper request to Onliner, return list of flats"""
     url_base = BASE_API_URL
 
-    def send_search_request(self, url_params: QueryParams) -> List[Flats]:
-        pass
-
-    def parse_url_params_from_site(self, public_url: str) -> None:
+    @staticmethod
+    def parse_url(public_url: str) -> None:
         """
         :type public_url: str URL which user can see in browser window
         """
         parsed_url = urlparse(public_url)
         query = parse_qs(parsed_url.query)
         coordinates = parse_qs(parsed_url.fragment)
-        ConfigHandler.update_url_params(QueryParams(
-            rent_type=query['rent_type[]'],
-            price_min=int(query['price[min]'][0]),
-            price_max=int(query['price[max]'][0]),
-            currency=query['currency'][0],
-            bounds_lb_lat=coordinates['bounds[lb][lat]'][0],
-            bounds_lb_long=coordinates['bounds[lb][long]'][0],
-            bounds_rt_lat=coordinates['bounds[rt][lat]'][0],
-            bounds_rt_long=coordinates['bounds[rt][long]'][0],
-            page=1
-        ))
+        query.update(coordinates)
+        SearchConfig.write(query)
 
-    def make_search_url(self, url_params: QueryParams) -> QueryUrl:
+    @staticmethod
+    def make_search_url(url_params: dict) -> QueryUrl:
         return QueryUrl(url_base=BASE_API_URL,
-                        params=asdict(url_params))
+                        params=url_params)
 
     # todo: open notification with osascript -e 'display notification "Lorem ipsum dolor sit amet" with title "Title"'
--- test_onliner.py
+++ test_onliner.py
@@ -1,10 +1,9 @@
 import json
-from dataclasses import asdict
-from unittest import TestCase, skip
+from unittest import TestCase
 from unittest.mock import patch, mock_open
 
 from constants import BASE_API_URL, URL_PARAMS_FILE
-from new_flat import Onliner, QueryParams, QueryUrl, ConfigHandler
+from new_flat import Onliner, QueryUrl, SearchConfig
 
 
 class TestOnliner(TestCase):
@@ -16,42 +15,39 @@ class TestOnliner(TestCase):
                         '53.89240290447646&bounds%5Brt%5D%5Blong%5D=27.740993499755863'
         self.query_url = 'https://ak.api.onliner.by/search/apartments?rent_type%5B%5D=3_rooms&rent_type%5B%5D=4_rooms&rent_type%5B%5D=5_rooms&rent_type%5B%5D=6_rooms&price%5Bmin%5D=250&price%5Bmax%5D=540&currency=usd&bounds%5Blb%5D%5Blat%5D=53.80876065067746&bounds%5Blb%5D%5Blong%5D=27.6551628112793&bounds%5Brt%5D%5Blat%5D=53.89240290447646&bounds%5Brt%5D%5Blong%5D=27.740993499755863'
 
-        example_params = QueryParams(
-            rent_type=['3_rooms', '4_rooms', '5_rooms', '6_rooms'],
-            price_min=250,
-            price_max=540,
-            currency='usd',
-            bounds_lb_lat='53.80876065067746',
-            bounds_lb_long='27.6551628112793',
-            bounds_rt_lat='53.89240290447646',
-            bounds_rt_long='27.740993499755863',
-        )
-        self.example_params = example_params
-        self.example_json_params = json.dumps(asdict(example_params))
+        example_params_dict = {
+            'rent_type[]': ['3_rooms', '4_rooms', '5_rooms', '6_rooms'],
+            'price[min]': ['250'],
+            'price[max]': ['540'],
+            'currency': ['usd'],
+            'bounds[lb][lat]': ['53.80876065067746'],
+            'bounds[lb][long]': ['27.6551628112793'],
+            'bounds[rt][lat]': ['53.89240290447646'],
+            'bounds[rt][long]': ['27.740993499755863'],
 
+        }
+        self.example_params_dict = example_params_dict
+        self.example_json_params = json.dumps(example_params_dict)
 
-    @skip
-    def test_send_search_request(self):
-        self.fail()
-
-    @patch('new_flat.ConfigHandler.update_url_params')
-    def test_parse_url_params_from_site(self, update_url_params_mock):
-        Onliner().parse_url_params_from_site(self.site_url)
-        update_url_params_mock.assert_called_once_with(self.example_params)
+    @patch('new_flat.SearchConfig.write')
+    def test_parse_url_to_dict(self, write_url_params):
+        Onliner().parse_url(self.site_url)
+        self.assertDictEqual(write_url_params.call_args[0][0], self.example_params_dict)
 
     @patch('new_flat.open', new_callable=mock_open)
     @patch('new_flat.json.dump')
-    def test_update_url_params(self, mock_dump, mocked_open):
-        ConfigHandler().update_url_params(self.example_params)
-        mock_dump.assert_called_once_with(asdict(self.example_params), mocked_open.return_value)
+    def test_write_url_params(self, mock_dump, mocked_open):
+        SearchConfig().write(self.example_params_dict)
+        mock_dump.assert_called_once_with(self.example_params_dict,
+                                          mocked_open.return_value)
 
     def test_make_search_url(self):
-        result = Onliner().make_search_url(self.example_params)
-        self.assertEqual(result, QueryUrl(BASE_API_URL, asdict(self.example_params)))
+        result = Onliner().make_search_url(self.example_params_dict)
+        self.assertEqual(result, QueryUrl(BASE_API_URL, self.example_params_dict))
 
     def test_read_url_params(self):
         """ should read from file and return QS instance """
         with patch('new_flat.open', mock_open(read_data=self.example_json_params)) as m:
-            result = ConfigHandler.read_url_params()
+            result = SearchConfig.read()
         m.assert_called_once_with(URL_PARAMS_FILE)
-        self.assertEqual(result, self.example_params)
+        self.assertEqual(result, self.example_params_dict)
